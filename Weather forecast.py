f8bc8a9d2eee2a21ff94c6df1ac2ce5c512fa473
marqq-qq <168688250+marqq-qq@users.noreply.github.com>
2024-12-25 20:59:35 +0300

    Create Weather forecast.py

---
+++ Weather forecast.py
@@ -0,0 +1,23 @@
+import requests
+
+API_KEY = "your_api_key"
+city = "your_city"
+country_code = "your_country_code"
+language = "your_language"
+
+url_city_coordinates = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{country_code}&appid={API_KEY}"
+city_data = requests.get(url_city_coordinates).json()
+latitude = city_data[0]['lat']
+longitude = city_data[0]['lon']
+
+url_weather = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}&units=metric&lang={language}"
+weather_data = requests.get(url_weather).json()
+
+main_weather = weather_data['weather'][0]['main']
+temperature = format(float(weather_data['main']['temp']), ".1f")
+feels_like_temp = format(float(weather_data['main']['feels_like']), ".1f")
+pressure = weather_data['main']['pressure']
+wind_speed = weather_data['wind']['speed']
+
+print(f"There is/are {main_weather} in {city} now, the temperature is {temperature} degrees Celsius, it feels like {feels_like_temp}\n"
+      f"pressure is {pressure} mmHg, wind speed is {wind_speed} m/s")
